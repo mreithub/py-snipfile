441bfdde06c4f6b79a566459c5d8b6c7d5b3809e
Manuel Reithuber <manuel@reithuber.net>
2023-08-13 20:57:14 +0200

    renamed project to snipfile

--- tests/test_slice.py
+++ tests/test_slice.py
@@ -1,10 +1,9 @@
-import io
 import os
 
-import binfile
+import snipfile
 
 def test_seek():
-    slice = binfile.Slice(binfile.fromBytes(b'hello, my dear world'), offset=7, size=7) # "my dear"
+    slice = snipfile.Slice(snipfile.fromBytes(b'hello, my dear world'), offset=7, size=7) # "my dear"
     assert slice.read() == b'my dear'
     assert slice.read() == b''
 
@@ -33,38 +32,38 @@ def test_seek():
 
 def test_split():
     data = b'a;b'
-    parts = [slice.read() for slice in binfile.split(binfile.fromBytes(data), b';')]
+    parts = [slice.read() for slice in snipfile.split(snipfile.fromBytes(data), b';')]
     assert parts == data.split(b';')
 
     data = b';'
-    parts = [slice.read() for slice in binfile.split(binfile.fromBytes(data), b';')]
+    parts = [slice.read() for slice in snipfile.split(snipfile.fromBytes(data), b';')]
     assert parts == data.split(b';')
 
     data = b';;'
-    parts = [slice.read() for slice in binfile.split(binfile.fromBytes(data), b';')]
+    parts = [slice.read() for slice in snipfile.split(snipfile.fromBytes(data), b';')]
     assert parts == data.split(b';')
 
     data = b'hello world, this.is.a test '
-    parts = [slice.read() for slice in binfile.split(binfile.fromBytes(data), b" ")]
+    parts = [slice.read() for slice in snipfile.split(snipfile.fromBytes(data), b" ")]
     assert parts == data.split(b' ')
 
     data = b'bananabread'
-    parts = [slice.read() for slice in binfile.split(binfile.fromBytes(data), b'an')]
+    parts = [slice.read() for slice in snipfile.split(snipfile.fromBytes(data), b'an')]
     assert parts == data.split(b'an')
 
 def test_split_long_delimiter():
     # delimiter's longer than the file
     data = b'abc'
-    parts = [slice.read() for slice in binfile.split(binfile.fromBytes(data), b'thisislongerthanbuff')]
+    parts = [slice.read() for slice in snipfile.split(snipfile.fromBytes(data), b'thisislongerthanbuff')]
     assert parts == [b'abc']
 
     data = b''
-    parts = [slice.read() for slice in binfile.split(binfile.fromBytes(data), b';')]
+    parts = [slice.read() for slice in snipfile.split(snipfile.fromBytes(data), b';')]
     assert parts == [b'']
 
 def test_splitAfter():
     data = b'hello\nworld\n'
-    parts = [slice.read() for slice in binfile.splitAfter(binfile.fromBytes(data), b'\n')]
+    parts = [slice.read() for slice in snipfile.splitAfter(snipfile.fromBytes(data), b'\n')]
     assert parts == [b'hello\n', b'world\n'] # TODO I think the last item should be omitted for splitAfter() - 
 
 #def test_splitBefore():
